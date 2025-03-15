8d031909e2450a4b9915b03650d67259b52c1006
xs19940230 <westdesperadolife@gmail.com>
2025-03-15 10:03:38 +0800

    fix v0.1

--- sever.js
+++ sever.js
@@ -14,17 +14,30 @@ app.get('/', (req, res) => {
 });
 
 let users = [];
+let messageQueue = [];
+const Max_Messages = 50;
 
 // Socket.io 连接事件。
 //为io设置一个事件监听器
 io.on('connection', (socket) =>
     {console.log('user connected!');
+        //发送历史消息
+        socket.emit('history',messageQueue);
+
         //接受客户端消息，并广播
         socket.on('chatMessage', (msg) => {
+            if (messageQueue.length >= Max_Messages) {
+                messageQueue.shift();
+            }
+            messageQueue.push(msg);
+            console.log('收到消息：',msg,'队列长度：',messageQueue.length);
             io.emit('chatMessage', msg);
         });
     socket.on('disconnect', () =>
     {console.log('user disconnected!');});
+    socket.on('error', (err) => {
+        console.log('Socket error: ',err);
+    });
 });
 
 // 启动服务器
